70f7b045d88b0547631f6bc5632c4e3cbd02bfa0
Marcin Wojtach <marcin.vojtach@gmail.com>
2025-03-31 14:06:54 +0200

    feat: add color to file icon in statusline

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -8,6 +8,19 @@ vim.o.statusline = "%f %h%w%m%r%=%-14.(%l,%c%V%) %P"
 See `:h "statusline'` for more information about statusline.
 ]]
 
+local devicons = require "nvim-web-devicons"
+
+local bg_color = "#343B4B"
+local fg_color = "#E5E9F0"
+
+vim.api.nvim_set_hl(0, "Statusline", { fg = fg_color, bg = bg_color })
+
+local function set_icon_color(color)
+  vim.api.nvim_set_hl(0, "IconColor", { fg = color, bg = bg_color })
+end
+
+set_icon_color(fg_color)
+
 ---@return string
 local function lsp_attached()
   local attached_clients = vim.lsp.get_clients({ bufnr = 0 })
@@ -57,14 +70,16 @@ local function lsp_status()
   return table.concat(status, " ")
 end
 
+---@return string
 local function file_route()
   local file_name = vim.fn.expand("%:t")
   local work_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ':t');
-  local file_icon = require "nvim-web-devicons".get_icon_by_filetype(vim.bo.filetype)
-  local icon = file_icon or ""
+  local icon, color = devicons.get_icon_color(file_name, vim.bo.filetype)
+  set_icon_color(color)
+  local file_icon = icon or ""
   if file_name ~= "" then
     return work_dir ..
-        ' / .. / ' .. file_name .. ' ' .. icon
+        "%#Statusline# / .. / " .. file_name .. " %#IconColor#" .. file_icon .. "%#Statusline#"
   end
 
   return work_dir
@@ -87,11 +102,7 @@ function _G.statusline()
   }, " ")
 end
 
-local bg_status = "#343B4B"
-
 vim.o.statusline = "%{%v:lua._G.statusline()%}"
-
-vim.api.nvim_set_hl(0, "Statusline", { fg = "#E5E9F0", bg = bg_status })
 
 vim.api.nvim_create_autocmd("DiagnosticChanged", {
   pattern = "*",
